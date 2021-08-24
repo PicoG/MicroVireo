d4cf8a6341c919a1ba8691807ec38983a7528d9e
Derrick Bommarito <negentropicdev@gmail.com>
2021-08-23 23:38:23 -0500

    Build config mostly refactored. RP2040 Pi Pico build config is now mainly just Pi Pico SDK specific config and Vireo feature config.

--- platform/picog/picog.cmake
+++ platform/picog/picog.cmake
@@ -1,3 +1,45 @@
+
+
+set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Set build type to reduce firmware size
+# Used by pico-sdk
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE MinSizeRel)
+endif()
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+#See if PICK_SDK_PATH already defined by environment, otherwise set it to default
+if (DEFINED ENV{PICO_SDK_PATH})
+    set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
+else ()
+    #default to looking for pico-sdk as a sibling of the Vireo folder
+    get_filename_component(PICO_SDK_PATH "../../../pico-sdk" ABSOLUTE)
+    if (NOT EXISTS ${PICO_SDK_PATH}/pico_sdk_init.cmake)
+        message(FATAL_ERROR "PICO_SDK_PATH variable not set in environment.")
+    endif()
+endif()
+
+#determine vireo source root
+get_filename_component(VIREO_DIR "../../source" ABSOLUTE)
+
+# Set the board directory and check that it exists.
+if(NOT VIREO_BOARD_DIR)
+    set(VIREO_BOARD_DIR ${CMAKE_SOURCE_DIR}/boards/${VIREO_BOARD})
+endif()
+if(NOT EXISTS ${VIREO_BOARD_DIR}/vireoboard.cmake)
+    message(FATAL_ERROR "Invalid VIREO_BOARD specified: ${VIREO_BOARD}")
+endif()
+
+# Include board config which brings in board specific pinouts and peripherals
+include(${VIREO_BOARD_DIR}/vireoboard.cmake)
+
+# Include the configuration for vireo and the main vireo source
+include(${CMAKE_CURRENT_LIST_DIR}/vireo_config.cmake)
+include(${VIREO_DIR}/vireo.cmake)
+
 include(${CMAKE_CURRENT_LIST_DIR}/version.cmake)
 
 set(BUILD_HEADER "${CMAKE_CURRENT_LIST_DIR}/include/picog/build.h")
@@ -19,8 +61,12 @@ add_custom_command(
 )
 
 #set_picog_build is the main macro to configure a target as picoG firmware
-macro(set_picog_build target)
-    target_compile_definitions(${target}
+macro(create_picog_build)
+    set(PICOG_TARGET "picoG_${VIREO_PLATFORM}_${VIREO_BOARD}_${PICOG_VERSION}")
+
+    add_executable(${PICOG_TARGET})
+
+    target_compile_definitions(${PICOG_TARGET}
         PUBLIC _PICOG_PLATFORM=${VIREO_PLATFORM}
         PUBLIC _PICOG_BOARD=${VIREO_BOARD}
         PUBLIC PICOG_VER_MAJOR=${PICOG_VER_MAJOR}
@@ -28,5 +74,15 @@ macro(set_picog_build target)
         PUBLIC PICOG_VER_PATCH=${PICOG_VER_PATCH}
     )
 
-    add_dependencies(${target} picoG_Version)
+    target_sources(${PICOG_TARGET} PUBLIC
+        ${VIREO_SOURCE_CORE}
+        ${VIREO_SOURCE_IO}
+    )
+
+    target_include_directories(${PICOG_TARGET}
+        PUBLIC ${VIREO_INCLUDE}
+        PUBLIC ${CMAKE_CURRENT_LIST_DIR}/../picog/include
+    )
+
+    add_dependencies(${PICOG_TARGET} picoG_Version)
 endmacro()
